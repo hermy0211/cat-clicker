b977c8674d222553944b2a0380e6395906381c90
Anne Lee <hermy0211@naver.com>
2018-07-15 23:12:31 +0900

    Updated for better scalability

--- js/app.js
+++ js/app.js
@@ -1,35 +1,44 @@
-// Global variables
+// Define global variables
+const numCats = 3;
+const catUrls = ["http://notgoaway.com/wp-content/uploads/2017/08/Cute-Cat-80-Go.jpg", "https://i.pinimg.com/originals/c4/4d/99/c44d996ee46a1d39fe18aaa8fef06dec.jpg", "https://wallpaperstock.net/curious-cat-wallpapers_33445_1600x1200.jpg"];
+const catNames = ["First Cat", "Second Cat", "Third Cat"];
+
 const counterText = document.querySelector('.counter');
-const images = document.querySelector('.images');
-let counter = 0;
+const image = document.querySelector('.image');
+const sidebar = document.querySelector('.cat-list-items');
 
-// Add another cat image
-const secondCat = document.createElement('span');
-secondCat.classList = "individual-image"
-secondCat.innerHTML = `<img src="https://i.pinimg.com/originals/c4/4d/99/c44d996ee46a1d39fe18aaa8fef06dec.jpg" alt="Second Cat" class="cat-image">`;
-images.appendChild(secondCat);
+let counter = [0, 0, 0];
 
-// Define cat image variable
-const catImages = document.querySelectorAll('.cat-image');
-const catImage = document.querySelectorAll('.individual-image')
+// Load the selected cat onto the page
+function loadCat(index) {
+  // Show cat image
+  image.innerHTML = "";
+  let cat = document.createElement('span');
+  cat.classList = "cat-image";
+  cat.innerHTML = `\<img src=\"${catUrls[index]}\" alt=\"${catNames[i]}\" class=\"cat-image\"\>`;
+  image.appendChild(cat);
 
-// Add event listeners for each image
-catImage[0].addEventListener('click', function() {
-  counter++;
-  counterText.textContent = counter;
-}, false);
+  // Show image title
+  const title = document.createElement('p');
+  title.classList = "image-title";
+  title.innerHTML = `${catNames[index]}`;
+  image.appendChild(title);
 
-catImage[1].addEventListener('click', function() {
-  counter++;
-  counterText.textContent = counter;
-}, false);
+  // Reset counter
+  counterText.textContent = counter[index];
 
-// Add titles on top of images
-const firstTitle = document.createElement('span');
-const secondTitle = document.createElement('span');
-firstTitle.classList = "image-title";
-secondTitle.classList = "image-title";
-firstTitle.innerHTML = "First Cat";
-secondTitle.innerHTML = "Second Cat";
-catImage[0].appendChild(firstTitle);
-catImage[1].appendChild(secondTitle);
+  // Add event listener for image
+  let catImage = document.querySelector('.cat-image');
+  catImage.addEventListener('click', function() {
+    counter[index]++;
+    counterText.textContent = counter[index];
+  }, false);
+}
+
+// Add cats to sidebar
+for (i=0; i<numCats; i++) {
+  let newCat = document.createElement('li');
+  newCat.innerHTML = `${catNames[i]}`
+  newCat.addEventListener('click', loadCat.bind(this, i));
+  sidebar.appendChild(newCat);
+}
